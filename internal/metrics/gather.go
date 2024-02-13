1416e8d387ab49c752465b44b718317353e65e76
Emil H. Clausen <sequoiia@hummel.yt>
2024-02-13 13:35:30 +0100

    Made sure data is mapped by cluster id

--- internal/metrics/gather.go
+++ internal/metrics/gather.go
@@ -25,22 +25,22 @@ type MetricData struct {
 }
 
 type AllMetricsResponse struct {
-	Days30 map[MetricKey]map[string]float64
-	PerDay map[MetricKey]map[string][]MetricData
+	Days30 map[MetricKey]map[ClusterId]map[string]float64
+	PerDay map[MetricKey]map[ClusterId]map[string][]MetricData
 }
 
 func (g *Gatherer) GetAllMetrics() *AllMetricsResponse {
-	dataStore30Days := make(map[MetricKey]map[string]float64)
-	dataStorePerDay := make(map[MetricKey]map[string][]MetricData)
+	dataStore30Days := make(map[MetricKey]map[ClusterId]map[string]float64)
+	dataStorePerDay := make(map[MetricKey]map[ClusterId]map[string][]MetricData)
 	now := time.Now()
 
 	for _, metricKey := range ConfluentMetrics {
 		// check if metricKey exists in dataStore30Days and dataStorePerDay, if not, init
 		if _, ok := dataStore30Days[metricKey]; !ok {
-			dataStore30Days[metricKey] = make(map[string]float64)
+			dataStore30Days[metricKey] = make(map[ClusterId]map[string]float64)
 		}
 		if _, ok := dataStorePerDay[metricKey]; !ok {
-			dataStorePerDay[metricKey] = make(map[string][]MetricData)
+			dataStorePerDay[metricKey] = make(map[ClusterId]map[string][]MetricData)
 		}
 
 		baseQuery := fmt.Sprintf("sum_over_time(%s[1d]", metricKey)
@@ -67,16 +67,23 @@ func (g *Gatherer) GetAllMetrics() *AllMetricsResponse {
 			}
 
 			for _, vector := range data {
-				if _, ok := dataStorePerDay[metricKey][vector.Metric.Topic]; !ok {
-					dataStorePerDay[metricKey][vector.Metric.Topic] = []MetricData{}
+				if _, ok := dataStorePerDay[metricKey][ClusterId(vector.Metric.KafkaID)]; !ok {
+					dataStorePerDay[metricKey][ClusterId(vector.Metric.KafkaID)] = map[string][]MetricData{}
 				}
+				if _, ok := dataStorePerDay[metricKey][ClusterId(vector.Metric.KafkaID)][vector.Metric.Topic]; !ok {
+					dataStorePerDay[metricKey][ClusterId(vector.Metric.KafkaID)][vector.Metric.Topic] = []MetricData{}
+				}
+				if _, ok := dataStore30Days[metricKey][ClusterId(vector.Metric.KafkaID)]; !ok {
+					dataStore30Days[metricKey][ClusterId(vector.Metric.KafkaID)] = map[string]float64{}
+				}
+
 				f64, _ := strconv.ParseFloat(vector.Value.Value, 64)
-				dataStorePerDay[metricKey][vector.Metric.Topic] = append(dataStorePerDay[metricKey][vector.Metric.Topic], MetricData{
+
+				dataStorePerDay[metricKey][ClusterId(vector.Metric.KafkaID)][vector.Metric.Topic] = append(dataStorePerDay[metricKey][ClusterId(vector.Metric.KafkaID)][vector.Metric.Topic], MetricData{
 					Time:  float64(timestamp.Unix()),
 					Value: f64,
 				})
-
-				dataStore30Days[metricKey][vector.Metric.Topic] = dataStore30Days[metricKey][vector.Metric.Topic] + f64
+				dataStore30Days[metricKey][ClusterId(vector.Metric.KafkaID)][vector.Metric.Topic] = dataStore30Days[metricKey][ClusterId(vector.Metric.KafkaID)][vector.Metric.Topic] + f64
 			}
 		}
 	}
